120e7d345dc1f51f069a15dd017264f835ab414d
madhumedisetty <108189103+madhumedisetty@users.noreply.github.com>
2022-12-18 18:38:09 +0530

    Time: 124 ms (19.30%), Space: 14.1 MB (16.62%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,6 +1,18 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        a=collections.Counter(nums1)
-        b=collections.Counter(nums2)
-        return (a&b).elements()
+        a=dict(Counter(nums1))
+        b=dict(Counter(nums2))
+        a_keys=list(a)
+        b_keys=list(b)
+        com=[]
+        if len(a_keys) >= len(b_keys):
+            for i in a_keys:
+                if i in b_keys:
+                    com+=[i]*min(a[i],b[i])
+        else:
+            for i in b_keys:
+                if i in a_keys:
+                    com+=[i]*min(a[i],b[i])
+        return com
+         
         
